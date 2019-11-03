3b4b9956f086eb2604a947f8d688c383240a630e
Caio Almeida <caioalmeidanaweb@gmail.com>
2019-11-02 23:31:25 -0400

    conclusão do minicurso

--- routes/admin.js
+++ routes/admin.js
@@ -11,12 +11,29 @@ router.get('/', function(req, res, next) {
   res.render('admin/index', { matches });
 });
 
-router.get('match/:id', function(req,res,next){
+router.get('/match/:id', function(req,res,next){
   const matches = db.get('matches').value()
-  const match =  db.get('match['+req.params.id+']').value()
+  const match =  db.get('matches['+req.params.id+']').value()
 
   res.render('admin/match',{match,matches,id:req.params.id})
 })
+
+router.post('/match/:id/score',function(req,res,next){
+  db.set('matches['+ req.params.id +'].team-a.score',parseInt(req.body.scoreA)).write()
+  db.set('matches['+ req.params.id +'].team-b.score',parseInt(req.body.scoreB)).write()
+
+  io.emit('score',{
+    match : req.params.id,
+    scoreA : req.body.scoreA,
+    scoreB : req.body.scoreB,
+    notify : req.body.notify || 0
+  })
+
+  res.send(req.body)
+})
+
+
+ 
 
 return router;
 }
